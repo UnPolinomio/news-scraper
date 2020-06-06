5e9cabac77a1448506b392580547c5fe39fa5d84
Hector Franco T <tor6.franco6@hotmail.com>
2020-06-05 21:44:17 -0500

    Slugify news title

--- scraper/utils/output.py
+++ scraper/utils/output.py
@@ -1,5 +1,6 @@
 import os
 import datetime
+from slugify import slugify
 from scraper.utils.config import get_config
 
 def save_to_file(scraped_article):
@@ -9,7 +10,8 @@ def save_to_file(scraped_article):
     if not os.path.exists(f'output/{today}'):
         os.mkdir(f'output/{today}')
 
-    with open(f'output/{today}/{scraped_article["title"]}', mode='w', encoding='utf-8') as f:
+    filename = f'output/{today}/{slugify(scraped_article["title"])}'
+    with open(filename, mode='w', encoding='utf-8') as f:
         f.write(scraped_article['title'])
         f.write('\n\n')
         f.write(scraped_article['summary'])
